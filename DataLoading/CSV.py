a3d8f45de2cb77d9376410bb1e62041c70e82dc8
Frédéric CELERSE <45543646+Fredericcelerse@users.noreply.github.com>
2024-07-31 09:37:46 +0200

    Create CSV.py

---
+++ DataLoading/CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+def load_data_csv(path: str):
+    """
+    Load CSV files.
+    
+    :param path: Path to the CSV file
+    :return: Tuple of panda DataFrame (data)
+    """
+    data = pd.read_csv(path)
+    return data
+
+def print_head(data: pd.DataFrame, n: int = 5):
+    """
+    Print the first n rows of the DataFrame.
+    
+    :param data: DataFrame to print
+    :param n: Number of rows to print, default is 5
+    """
+    print(data.head(n))
