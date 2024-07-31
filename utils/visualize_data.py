425cc9b4a2c764a7800d504c011235c5765b9d91
Frédéric CELERSE <45543646+Fredericcelerse@users.noreply.github.com>
2024-07-31 10:15:52 +0200

    Update visualize_data.py

--- utils/visualize_data.py
+++ utils/visualize_data.py
@@ -14,3 +14,32 @@ def visualize_missing_data(data: pd.DataFrame):
     sns.heatmap(data.isnull(), cbar=False, cmap='viridis')
     plt.title('Heatmap of missing data')
     plt.show()
+
+def visualize_specific_data(data: pd.DataFrame, feature_name: str, bins: int = 20):
+    """
+    Visualize only one feature of a DataFrame.
+
+    :param data: DataFrame to visualize
+    :param feature_name: Name of the feature to visualize
+    :param bins: Number of bins
+    """
+    sns.histplot(data[feature_name], bins=bins, kde=True)
+    plt.title(f'Distribution of {feature_name}')
+    plt.xlabel(feature_name)
+    plt.ylabel('Frequency')
+    plt.show()
+
+def visualize_multiple_data(data: pd.DataFrame, num_rows: int = None):
+    """
+    Visualize mutliple data at once. Optionally, only a subset of the data can be visualized.
+
+    :param data: DataFrame to visualize
+    :param num_rows: Number of rows to visualize. If None, visualize all data.
+    """
+    if num_rows is not None:
+        data = data.head(num_rows)
+    
+    data.hist(figsize=(20, 15), bins=20)
+    plt.tight_layout()
+    plt.show()
+    
