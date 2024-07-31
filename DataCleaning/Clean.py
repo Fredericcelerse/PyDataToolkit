66f98f36caa4ff392dc27c10dc54f1f5844e264a
Frédéric CELERSE <45543646+Fredericcelerse@users.noreply.github.com>
2024-07-31 09:52:15 +0200

    Update Clean.py

--- DataCleaning/Clean.py
+++ DataCleaning/Clean.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 def check_missing_data(data: pd.DataFrame):
     """
@@ -22,3 +23,36 @@ def clean_missing_data(missing_data: pd.DataFrame):
     """
     cleaned_missing_data = missing_data.drop_duplicates()
     return cleaned_missing_data
+
+def drop_columns_with_missing_data(data: pd.DataFrame, threshold: float = 0.5):
+    """
+    Drop columns where missing data are higher than the defined threshold
+
+    :param dat: DataFrame
+    :param threshold: Threshold we define manually
+    :return: Cleaned data
+    """
+    data = data[data.columns[data.isnull().mean() < threshold]]
+    return data
+
+def impute_missing_values(data: pd.DataFrame):
+    """
+    Impute missing values in the DataFrame.
+    Float and In are filled using the mean approach
+    Object are filled using the most frequent approach
+    
+    :param data: DataFrame to impute
+    :return: DataFrame with imputed values
+    """
+    # Impute missing values for numerical columns
+    numeric_cols = data.select_dtypes(include=[float, int]).columns
+    imputer_mean = SimpleImputer(strategy='mean')
+    data[numeric_cols] = imputer_mean.fit_transform(data[numeric_cols])
+    
+    # Impute missing values for categorical columns
+    categorical_cols = data.select_dtypes(include=[object]).columns
+    imputer_mode = SimpleImputer(strategy='most_frequent')
+    data[categorical_cols] = imputer_mode.fit_transform(data[categorical_cols])
+    
+    return data
+
